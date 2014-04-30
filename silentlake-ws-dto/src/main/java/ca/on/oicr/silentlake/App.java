47df20bed2fd7fb82d90db66531c73a2025dbef8
mnerandzic <Marko.Nerandzic@oicr.on.ca>
2014-04-30 15:21:53 -0400

    Initial commit

---
+++ silentlake-ws-dto/src/main/java/ca/on/oicr/silentlake/App.java
@@ -0,0 +1,13 @@
+package ca.on.oicr.silentlake;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
