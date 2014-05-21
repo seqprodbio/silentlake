6b6a312e7e10f7824f9402d1d5cfe3e2f2926ed0
Tony DeBat <tony.debat@oicr.on.ca>
2014-05-21 13:00:46 -0400

    Added experimentlibrarydesign resource.

---
+++ silentlake-ws/src/main/java/ca/on/oicr/silentlake/ws/ExperimentLibraryDesignResource.java
@@ -0,0 +1,10 @@
+package ca.on.oicr.silentlake.ws;
+
+import javax.ejb.Stateless;
+import javax.ws.rs.Path;
+
+@Stateless
+@Path("/experimentlibrarydesign")
+public class ExperimentLibraryDesignResource {
+
+}
