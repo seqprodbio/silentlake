e7cc687929cae1ec0a549c1f9732da683f7649f7
Tony DeBat <tony.debat@oicr.on.ca>
2014-05-28 12:08:56 -0400

    Added silentlake-model module to store custom models required to talk to the SeqWare database.

---
+++ silentlake-model/src/main/java/ca/on/oicr/silentlake/model/App.java
@@ -0,0 +1,13 @@
+package ca.on.oicr.silentlake.model;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
